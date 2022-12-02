f8924d290270f05d769c871059fedbb6c5d8c113
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2022-12-02 19:42:22 +0200

    Impilement Prim algo in Java

--- Prim's algorithm Java-Script.js
+++ Prim's algorithm Java-Script.js
@@ -51,3 +51,22 @@ primsMST() {
   }
   return MST;
 }
+
+let g = new Graph();
+g.addNode("A");
+g.addNode("B");
+g.addNode("C");
+g.addNode("D");
+g.addNode("E");
+g.addNode("F");
+g.addNode("G");
+
+
+g.addEdge("A", "C", 100);
+g.addEdge("A", "B", 3);
+g.addEdge("A", "D", 4);
+g.addEdge("C", "D", 3);
+g.addEdge("D", "E", 8);
+g.addEdge("E", "F", 10);
+g.addEdge("B", "G", 9);
+g.primsMST().display();
